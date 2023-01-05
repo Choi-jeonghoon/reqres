d2d9de682a017ce6e640def59e3617fac5d074af
최정훈 <jeongssi94@gmail.com>
2023-01-06 00:45:20 +0900

    Add:MainPage.map

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,11 +1,35 @@
 import { UserType } from '../types/userType';
+import { Typography, Card, CardMedia, CardContent } from '@mui/material';
 
 interface MainContainerPropsType {
   usersData: Array<UserType.User>;
 }
 const MainComponent = ({ usersData }: MainContainerPropsType) => {
   console.log(usersData);
-  return <div>마지막부분</div>;
+  return (
+    <div>
+      {usersData.map((item: UserType.User) => (
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia component='img' alt='green iguana' height='140' />
+          {item.avatar}
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='div'>
+              {item.first_name}
+            </Typography>
+            <Typography gutterBottom variant='h5' component='div'>
+              {item.last_name}
+            </Typography>
+            <Typography gutterBottom variant='h5' component='div'>
+              {item.email}
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              test
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default MainComponent;
